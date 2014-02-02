4079ad1063c0ebfbdd4c15a8eb33e5a8c0d71961
Mariuca <maria.mitica@gmail.com>
2014-02-02 15:27:07 +0200

    optimizations

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -11,11 +11,8 @@ class ExchangeController < ApplicationController
   end
 
   def convert
-    if (/^(\$)?((\d+)|(\d{1,3})(\,\d{3})*)(\.\d{1,})?$/).match(params[:amount])
-      original_exchange_rate = @file.css('Cube Cube Cube').select{|p| p['currency'] ===  params[:original_currency]}[0]['rate']
-      destination_exchane_rate = @file.css('Cube Cube Cube').select{|p| p['currency'] ===  params[:destination_currency]}[0]['rate']
-      @result = (params[:amount].gsub(',','').to_f * destination_exchane_rate.to_f / original_exchange_rate.to_f).round(2)
-      #@result = @eu_bank.exchange(params[:amount], params[:original_currency], params[:destination_currency])
+    if (/^(\$)?((\d+)|(\d{1,3})(\,\d{3})*)(\.\d+)?$/).match(params[:amount])
+      @result = @eu_bank.exchange_float(params[:amount], params[:original_currency], params[:destination_currency])
       respond_to do |format|
         format.js
       end
---
+++ config/initializers/eu_central_bank.rb
@@ -0,0 +1,20 @@
+require 'eu_central_bank'
+class EuCentralBank < Money::Bank::VariableExchange
+  alias_method :exchange_float, :exchange
+  alias_method :exchange_with_float, :exchange_with
+  def exchange_float(cents, from_currency, to_currency)
+    exchange_with_float(Money.new(cents, from_currency), to_currency)
+  end
+  def exchange_with_float(from, to_currency)
+    rate = get_rate(from.currency, to_currency)
+    unless rate
+      from_base_rate, to_base_rate = nil, nil
+      @mutex.synchronize {
+        from_base_rate = get_rate("EUR", from.currency, :without_mutex => true)
+        to_base_rate = get_rate("EUR", to_currency, :without_mutex => true)
+      }
+      rate = to_base_rate / from_base_rate
+    end
+    ((Money::Currency.wrap(to_currency).subunit_to_unit.to_f / from.currency.subunit_to_unit.to_f) * from.cents.to_f * rate).round(2)
+  end
+end
